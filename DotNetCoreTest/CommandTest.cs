01fcad178bf8fe608df5f369c36f3cc4bb13784c
Shariful-Adil <adil.ict@gmail.com>
2022-02-20 23:43:32 +0600

    xUnit test project added

---
+++ DotNetCoreTest/CommandTest.cs
@@ -0,0 +1,30 @@
+﻿using DotNetCoreAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DotNetCoreTest
+{
+    public class CommandTest
+    {
+        [Fact]
+        public void CanChangeHowTo()
+        {
+        //Arrange
+            var testCommand = new Command
+            {
+                HowTo = "Do something awesome",
+                Platform = "xUnit",
+                Line = "dotnet test"
+            };
+
+            //Act
+            testCommand.HowTo = "Execute Unit Tests";
+            testCommand.Platform = "xunit";
+            //Assert
+            Assert.Equal("Execute Unit Tests", testCommand.HowTo);
+            Assert.Equal("xunit", testCommand.Platform);
+        }
+    }
+}
